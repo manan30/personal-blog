a9edaf913b6872edb7cad49739a0e70718a57671
Manan Joshi <mananjoshi1995@gmail.com>
2020-09-06 02:04:11 -0500

    Adding social media icons

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,15 @@
 import styles from '../styles/Footer.module.css';
+import SocialIcons from './social-icons';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>
+      <div className={styles.row1}>
         <span>
-          &#169; Copyright 2020 <a href="https://mananjoshi.me">Manan Joshi</a>
+          <a href="https://mananjoshi.me">Manan Joshi</a> &#169; 2020
         </span>
+        <SocialIcons />
       </div>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by Vercel
-      </a>
     </footer>
   );
 }
---
+++ components/social-icons.js
@@ -0,0 +1,47 @@
+import InstagramIcon from '../public/images/svg/instagram.svg';
+import TwitterIcon from '../public/images/svg/twitter.svg';
+import EmailIcon from '../public/images/svg/email.svg';
+import GithubIcon from '../public/images/svg/github.svg';
+import LinkedInIcon from '../public/images/svg/linkedin.svg';
+import MediumIcon from '../public/images/svg/medium.svg';
+import StackOverflowIcon from '../public/images/svg/stackoverflow.svg';
+import styles from '../styles/SocialIcons.module.css';
+
+function SocialIcons() {
+  return (
+    <div className={styles.container}>
+      <a href="https://twitter.com/manan__joshi" aria-label="Visit my Twitter">
+        <TwitterIcon />
+      </a>
+      <a
+        href="https://www.instagram.com/manan__joshi/"
+        aria-label="Visit my Instagram"
+      >
+        <InstagramIcon />
+      </a>
+      <a href="https://github.com/manan30" aria-label="Visit my Github">
+        <GithubIcon />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mananjoshi1741/"
+        aria-label="Visit my LinkedIn"
+      >
+        <LinkedInIcon />
+      </a>
+      <a href="https://medium.com/@Manan_30" aria-label="Visit my Medium">
+        <MediumIcon />
+      </a>
+      <a
+        href="https://stackoverflow.com/users/5266912/manan-joshi"
+        aria-label="Visit my Stack Overflow"
+      >
+        <StackOverflowIcon />
+      </a>
+      <a href="mailto:mananjoshi1995@gmail.com" aria-label="Send me an email">
+        <EmailIcon />
+      </a>
+    </div>
+  );
+}
+
+export default SocialIcons;
