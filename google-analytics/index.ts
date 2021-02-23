29656bd4b911f16f1bfe661162aa4896f50b907d
Manan Joshi <mananjoshi1995@gmail.com>
2021-02-22 23:28:37 -0600

    Migrating google analytics file to Typescript

--- google-analytics/index.ts
+++ google-analytics/index.ts
@@ -1,6 +1,8 @@
 export const GA_TRACKING_ID = 'UA-168037223-4';
 
 export const pageView = (url) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url
   });
