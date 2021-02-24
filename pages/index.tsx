63c48140cd1689b101c82d4748ea2dcd6c2bbda1
Manan Joshi <mananjoshi1995@gmail.com>
2021-02-23 21:30:58 -0600

    Adding tailwind to layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,16 +24,16 @@ const Home: React.FC<HomePageProps> = ({ recentPosts, error }) => {
             "Manan Joshi is a passionate full-stack developer and loves working with web technologies. He is interested in building software that empowers the lives of people. He recently graduated with a Master's degree from Rochester Institute of Technology. He is currently working as a Software Engineer at Egen Solutions and lives in Naperville, Illinois."
         }}
       />
-      <div className={styles.container}>
-        <div className={styles.profileContainer}>
+      <div className="h-screen flex items-center justify-center flex-col">
+        <div className="flex flex-col items-center mx-12 mt-12">
           <img
-            className={styles.profilePicture}
+            className="h-32 w-32 rounded-full bg-center bg-no-repeat bg-contain"
             alt="Profile"
             src="/images/profile-picture.jpg"
             loading="lazy"
           />
-          <h1>Manan Joshi</h1>
-          <div className={styles.profileBio}>
+          <h1 className="text-4xl my-4 font-medium">Manan Joshi</h1>
+          <div className="text-center text-base">
             I try to explain the <i>chaos</i> in my mind with <i>ordered</i>{' '}
             code
           </div>
