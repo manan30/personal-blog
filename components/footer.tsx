3d2950841d62c9b03a7d386d901a7a6c2519c5bb
Manan Joshi <mananjoshi1995@gmail.com>
2021-02-23 20:45:23 -0600

    Adding tailwind to footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,9 @@
 import SocialIcons from './social-icons';
-import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   return (
     <footer className="w-full py-4 flex justify-center flex-col text-gray-500">
-      <div className={styles.row1}>
+      <div className="flex items-center justify-around">
         <span>
           <a href="https://mananjoshi.me">Manan Joshi</a> &#169;{' '}
           {new Intl.DateTimeFormat('en-US', { year: 'numeric' }).format(
