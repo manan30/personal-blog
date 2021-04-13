a39839182d58e4b7b1153eeb64c00bb58f15a9aa
Manan Joshi <mananjoshi1995@gmail.com>
2021-04-12 22:55:28 -0500

    Moving social icons component to tailwind

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import SocialIcons from './social-icons';
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 flex text-gray-500 mx-auto px-10 max-w-lg md:px-2 md:max-w-xl lg:max-w-4xl items-center flex-col md:flex-row dark:text-gray-100">
+    <footer className="w-full py-4 flex text-gray-500 mx-auto px-10 max-w-lg md:px-2 md:max-w-xl lg:max-w-4xl items-center flex-col md:flex-row dark:text-gray-100 transition duration-500">
       <span className="mb-3 md:mb-0">
         <a href="https://mananjoshi.me">Manan Joshi</a> &#169;{' '}
         {new Intl.DateTimeFormat('en-US', { year: 'numeric' }).format(
--- components/social-icons.tsx
+++ components/social-icons.tsx
@@ -5,39 +5,57 @@ import GithubIcon from '../public/images/svg/github.svg';
 import LinkedInIcon from '../public/images/svg/linkedin.svg';
 import MediumIcon from '../public/images/svg/medium.svg';
 import StackOverflowIcon from '../public/images/svg/stackoverflow.svg';
-import styles from '../styles/SocialIcons.module.css';
 
 const SocialIcons = () => {
   return (
-    <div className={styles.container}>
-      <a href="https://twitter.com/manan__joshi" aria-label="Visit my Twitter">
+    <div className="flex items-center space-x-4">
+      <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
+        href="https://twitter.com/manan__joshi"
+        aria-label="Visit my Twitter"
+      >
         <TwitterIcon />
       </a>
       <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
         href="https://www.instagram.com/manan__joshi/"
         aria-label="Visit my Instagram"
       >
         <InstagramIcon />
       </a>
-      <a href="https://github.com/manan30" aria-label="Visit my Github">
+      <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
+        href="https://github.com/manan30"
+        aria-label="Visit my Github"
+      >
         <GithubIcon />
       </a>
       <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
         href="https://www.linkedin.com/in/mananjoshi1741/"
         aria-label="Visit my LinkedIn"
       >
         <LinkedInIcon />
       </a>
-      <a href="https://medium.com/@Manan_30" aria-label="Visit my Medium">
+      <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
+        href="https://medium.com/@Manan_30"
+        aria-label="Visit my Medium"
+      >
         <MediumIcon />
       </a>
       <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
         href="https://stackoverflow.com/users/5266912/manan-joshi"
         aria-label="Visit my Stack Overflow"
       >
         <StackOverflowIcon />
       </a>
-      <a href="mailto:mananjoshi1995@gmail.com" aria-label="Send me an email">
+      <a
+        className="h-4 w-4 hover:text-blue-600 dark:hover:text-green-400 transition duration-500"
+        href="mailto:mananjoshi1995@gmail.com"
+        aria-label="Send me an email"
+      >
         <EmailIcon />
       </a>
     </div>
